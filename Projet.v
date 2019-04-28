ae4d753239b5ff195bb4534153bca8f82edb638f
ElisaHG <elisa.gueneau@icloud.com>
2019-04-28 21:41:58 +0200

    Fonctions OK, reste des lemmes a prouver

--- Projet.v
+++ Projet.v
@@ -1,18 +1,17 @@
 Require Import Nat.
 Require Import FunInd.
 Require Export ZArith. 
-Open Scope list.
-Require Export List.
-Import ListNotations.
+Require Import Classical.
 
 (* --- Arbre Binaire --- *)
 Inductive Abin : Set :=
 | Empty : Abin
 | Node : nat -> Abin -> Abin -> Abin.
 
-Compute (Node 5 (Node 6 Empty Empty) (Node 7 (Node 8 Empty Empty) Empty)).
+Compute (Node 5 (Node 6 Empty Empty) (Node 7 (Node 8 Empty Empty) Empty)).  (* exemple d'arbre binaire *)
 
-(*--------- Fonctions supplementaires qui utilisent les Arbres binaire --------------*)
+(*--------- Fonctions supplementaires qui utilisent les Arbres binaires -------------*)
+(* --- Taille Arbre Binaire --- *)
 Fixpoint HeightAbin (tree : Abin) : nat :=
 match tree with
 | Empty => 0
@@ -20,6 +19,7 @@ match tree with
 | (Node n l r) => 1 + (max (HeightAbin l) (HeightAbin r))
 end.
 
+(* --- nombre de noeuds dans Arbre Binaire --- *)
 Fixpoint nbNodeAbin (tree : Abin) : nat :=
 match tree with
 | Empty => 0
@@ -27,8 +27,8 @@ match tree with
 | (Node n l r) => 1 + (nbNodeAbin l) + (nbNodeAbin r)
 end.
 
-Compute (HeightAbin (Node 5 (Node 6 Empty Empty) (Node 7 (Node 8 Empty Empty) Empty))).
-Compute (nbNodeAbin (Node 5 (Node 6 Empty Empty) (Node 7 (Node 8 Empty Empty) Empty))).
+Compute (HeightAbin (Node 5 (Node 6 Empty Empty) (Node 7 (Node 8 Empty Empty) Empty))). (* arbre binaire de taille 3 *)
+Compute (nbNodeAbin (Node 5 (Node 6 Empty Empty) (Node 7 (Node 8 Empty Empty) Empty))). (* arbre binaire avec 4 noeuds *)
 (*-----------------------------------------------------------------------------------------------------*)
 
 (* --- Noeud --- *)
@@ -60,11 +60,9 @@ Inductive isABR : Abin -> Prop :=
 Lemma test_isABR : (isABR (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty)))).
 apply verifNodeABR. apply verifNodeABR. apply verifNodeABR.
 apply verifEmptyABR. apply verifEmptyABR.
-apply max_Empty.
-apply min_Empty.
+apply max_Empty. apply min_Empty.
 apply verifNodeABR. apply verifEmptyABR. apply verifEmptyABR.
-apply max_Empty.
-apply min_Empty.
+apply max_Empty. apply min_Empty.
 apply max_n.
 intros. inversion H. auto.
 inversion H4. inversion H4.
@@ -73,11 +71,9 @@ intros. inversion H. auto.
 inversion H4. inversion H4.
 apply verifNodeABR. apply verifNodeABR.
 apply verifEmptyABR. apply verifEmptyABR.
-apply max_Empty.
-apply min_Empty.
+apply max_Empty. apply min_Empty.
 apply verifNodeABR. apply verifEmptyABR. apply verifEmptyABR.
-apply max_Empty.
-apply min_Empty.
+apply max_Empty. apply min_Empty.
 apply max_n.
 intros. inversion H. auto.
 inversion H4. inversion H4.
@@ -98,7 +94,6 @@ apply min_n.
 intros. inversion H. auto.
 inversion H4. auto. inversion H9. inversion H9. inversion H4. omega.
 inversion H9. inversion H9.
-Qed.
 
 Ltac tactique_isABR :=
 repeat (auto || omega ||
@@ -125,8 +120,6 @@ Lemma testTac_isABR : (isABR (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty 
 tactique_isABR.
 Qed.
 
-(*Lemma test_isntABR : ~(isABR (Node 5 (Node 3 Empty Empty) (Node 4 Empty Empty))).*)
-
 (* --- Fonction de recherche d'un element dans un ABR --- *)
 Fixpoint search (arb : Abin) (x : nat) : Prop :=
 match arb with
@@ -142,8 +135,8 @@ match arb with
     end
 end.
 
-Compute (search (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 3).
-Compute (search (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 10).
+Compute (search (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 3). (* 3 present dans l'arbre *)
+Compute (search (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 10).  (* 10 absent de l'arbre *)
 
 (* --- Fonction d'insertion d'un element dans un ABR --- *)
 Fixpoint insert (arb : Abin) (x : nat) : Abin :=
@@ -201,11 +194,12 @@ match arb with
   end
 end.
 
-Compute (searchMaxABR (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty)))).
-Compute (searchMinABR (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty)))).
+Compute (searchMaxABR (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty)))). (* max = 11 *)
+Compute (searchMinABR (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty)))). (* min = 2 *)
 Compute (suppr (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 5).
 Compute (suppr (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 3).
 Compute (suppr (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 8 (Node 6 Empty Empty) (Node 11 Empty Empty))) 8).
+
 
 
 Functional Scheme searchInductive := Induction for search Sort Prop.
@@ -213,38 +207,87 @@ Functional Scheme insertInductive := Induction for insert Sort Prop.
 Functional Scheme supprInductive := Induction for suppr Sort Prop.
 
 (* --- PREUVES --- *)
-Lemma proof_search_correction : forall (a : Abin) (n: nat), (search a n) -> (isNode a n).
+(* si on trouve n dans l'arbre alors n est un noeud de l'arbre *)
+Lemma search_correction : forall (a : Abin) (n: nat), (search a n) -> (isNode a n).
 intro.
 intro.
 functional induction (search a n) using searchInductive;intros.
 inversion H.
 apply verifCurrentNode.
-apply verifLeft with x.
+apply verifLeft.
+auto.
+apply verifRight.
+auto.
+
+(* quand on insere un noeud dans un arbre, n a bien la propriete de noeud *)
+Lemma is_node_insert : forall (n : nat) (a : Abin), (isNode (insert a n) n).
+intro. intro.
+functional induction (insert a n) using insertInductive;intros.
+apply verifCurrentNode.
+apply verifLeft. apply IHa0.
+apply verifLeft. apply IHa0.
+apply verifRight. apply IHa0.
+
+Lemma insert_missing_correction : forall (n m : nat) (a: Abin), (isNode (insert a n) m) -> (n <>m) -> (isNode a m).
+intro. intro. intro.
+functional induction (insert a n) using insertInductive;intros.
+inversion H. omega.
+apply H5. apply H5.
+inversion H. omega.
+apply verifLeft. apply IHa0. apply H5. apply H0.
+apply verifRight.
+assumption.
+inversion H; subst.
+apply verifCurrentNode.
+apply verifLeft. apply IHa0. apply H5. apply H0.
+apply verifRight. apply H5.
+inversion H; subst.
+apply verifCurrentNode.
+apply verifLeft. apply H5.
+apply verifRight. apply IHa0. apply H5. apply H0.
+
+(* si m est un noeud de l'arbre , m est un noeud de l'arbre auquel on a ajoute un nouveau noeud n *)
+Lemma insert_missing_completude : forall (n m : nat) (a: Abin), (isNode a m) -> (isNode (insert a n) m).
+intros.
+elim H; intros.
+simpl.
+elim (Nat.eq_dec n n0); elim (lt_dec n n0);intros.
+apply verifCurrentNode.
+apply verifCurrentNode.
+apply verifCurrentNode.
+apply verifCurrentNode.
+simpl.
+elim (Nat.eq_dec n n0); elim (lt_dec n n0);intros.
+apply verifLeft. apply H1.
+apply verifLeft. apply H1.
+apply verifLeft. apply H1.
+apply verifLeft. apply H0.
+simpl.
+elim (Nat.eq_dec n n0); elim (lt_dec n n0);intros.
+apply verifRight. apply H0.
+apply verifRight. apply H0.
+apply verifRight. apply H0.
+apply verifRight. apply H1.
 
 
-Lemma proof_search_completude : forall (a: Abin) (n:nat), (isABR a) -> (isNode a n) -> (search a n).
+(* --------- Idees de preuves non abouties --------- *)
+(* Si n est un noeud de l'arbre, on pourra trouver n dans l'arbre *)
+Lemma search_completude : forall (a: Abin) (n:nat), (isABR a) -> (isNode a n) -> (search a n).
 
-Lemma proof_insert_is_node : forall (n : nat) (a : Abin), (isNode (insert a n) n).
+Lemma insert_unexpected_correction : forall (n m : nat) (a : Abin), (~(isNode (insert a n) m)) -> (n <>m) -> ~(isNode a m).
+Lemma insert_unexpected_completude : forall (n m : nat) (a : Abin), ~(isNode a m) -> ~(isNode (insert a n) m).
 
-Lemma proof_insert_missing_correction : forall (n m : nat) (a: Abin), ((n <>m) /\ (isNode (insert a n) m)) -> (isNode a m).
+(* Inserer une valeur plus petite que le max de l'arbre ne modifie pas le max *)
+Lemma insert_left_correction : forall (n m : nat) (a : Abin), (isMax (insert a n) m) -> (isMax a m). 
+Lemma insert_left_completude : forall (n m : nat) (a : Abin), (isMax a m) -> (n < m) -> (isMax (insert a n) m).
+(* Inserer une valeur plus grande que le min de l'arbre ne modifie pas le min *)
+Lemma insert_right_correction : forall (n m : nat) (a : Abin), (isMin (insert a n) m) -> (isMin a m).
+Lemma insert_right_completude : forall (n m : nat) (a : Abin), (isMin a m) -> (n > m) -> (isMin (insert a n) m).
 
-Lemma proof_insert_missing_completude : forall (n m : nat) (a: Abin), (isNode a m) -> (isNode (insert a n) m).
+(* Inserer un noeud dans un ABR conserve la propriete d'ABR *)
+Lemma insert_is_abr_corr : forall (n : nat) (a : Abin), (isABR (insert a n)) -> (isABR a).
+Lemma insert_is_abr_compl : forall (n : nat) (a : Abin), (isABR a) -> (isABR (insert a n)).
 
-Lemma proof_insert_unexpected_correction : forall (n m : nat) (a : Abin), ((n <>m) /\ ~(isNode (insert a n) m)) -> ~(isNode a m).
-
-Lemma proof_insert_unexpected_completude : forall (n m : nat) (a : Abin), ~(isNode a m) -> ~(isNode (insert a n) m).
-
-Lemma insert_left_abr_correction : forall (n m : nat) (left : Abin), ((n < m) /\ (isMax (insert left n) m)) -> (isMax left m).
-
-Lemma insert_left_abr_completude : forall (n m : nat) (left : Abin),  (isMax left m) -> (isMax (insert left n) m).
-
-Lemma insert_right_abr_correction : forall (n m : nat) (right : Abin), ((n > m) /\ (isMin (insert right n) m)) -> (isMin right m).
-
-Lemma insert_right_abr_completude : forall (n m : nat) (right : Abin), (isMin right m) -> (isMin (insert right n) m).
-
-Lemma insert_is_abr_correction : forall (n : nat) (a : Abin), (isABR (insert a n)) -> (isABR a).
-
-Lemma insert_is_abr_completude : forall (n : nat) (a : Abin), (isABR a) -> (isABR (insert a n)).
 
 
 
